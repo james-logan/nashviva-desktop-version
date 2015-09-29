fc1fa098e8e32da0318be83bd647ee71e733e3bb
James Logan <jamesblogan20@gmail.com>
2015-09-29 17:40:52 -0500

    angular-google-maps included and generating a map on the page

--- client/app/app.js
+++ client/app/app.js
@@ -5,13 +5,20 @@ angular.module('nashvivaDesktopApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'uiGmapgoogle-maps'
 ])
-  .config(function ($routeProvider, $locationProvider) {
+  .config(function ($routeProvider, $locationProvider, uiGmapGoogleMapApiProvider) {
     $routeProvider
       .otherwise({
         redirectTo: '/'
       });
 
     $locationProvider.html5Mode(true);
+
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyDFERo_-aBRo9DGiPEQwhejX_MSPG41wKM',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'geometry,visualization'
+    });
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('nashvivaDesktopApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, uiGmapGoogleMapApi) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
+    });
+
+    $scope.map = { center: {latitude: 36.16, longitude: -86.78}, zoom: 14, pan: false}
+
+    uiGmapGoogleMapApi.then(function (maps) {
+      console.log('shenanigans')
     });
 
   });
