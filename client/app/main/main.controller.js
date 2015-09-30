74dfeb363aeb6e000c3f9dfb3ef571523896bca3
James Logan <jamesblogan20@gmail.com>
2015-09-30 10:54:30 -0500

    grabbed second dataset

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,7 @@
 angular.module('nashvivaDesktopApp')
   .controller('MainCtrl', function ($scope, $http, uiGmapGoogleMapApi) {
     $scope.awesomeThings = [];
+    var vm = this;
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
@@ -18,10 +19,19 @@ angular.module('nashvivaDesktopApp')
       .get('https://data.nashville.gov/resource/dqkw-tj5j.json?$$app_token=8efm64PGcgXye0PGdUl0S2zw3')
       .then(function (data) {
         console.log(data.data)
+        vm.secondDataSet()
+
       }, function (err) {
         console.log(err)
       })
 
+      vm.secondDataSet = function () {
+        $http
+          .get('https://data.nashville.gov/resource/eviu-nxp6.json?$$app_token=8efm64PGcgXye0PGdUl0S2zw3')
+          .then(function (data) {
+            console.log(data.data)
+          }, function (err) {})
+      }
   });
 
 
