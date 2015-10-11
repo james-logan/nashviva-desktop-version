b24f3575b812e705b7c23fe5f590c652ab15010b
James Logan <jamesblogan20@gmail.com>
2015-10-11 18:38:39 -0500

    implemented login with facebook via passport module

--- server/app.js
+++ server/app.js
@@ -7,12 +7,18 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
+
 var express = require('express');
 var config = require('./config/environment');
+
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 require('./config/express')(app);
+
+
+
 require('./routes')(app);
 
 // Start server
--- server/routes.js
+++ server/routes.js
@@ -6,12 +6,39 @@
 
 var errors = require('./components/errors');
 var path = require('path');
+var secrets = require('./config/secrets')
+
+var FACEBOOK_APP_ID = secrets.FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET = secrets.FACEBOOK_APP_SECRET;
+
+
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+//setting up passport using Facebook strategy
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:9000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
 
 module.exports = function(app) {
 
   // Insert routes below
+
+  //Facebook auth route
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
   app.use('/api/things', require('./api/thing'));
-  
+
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
